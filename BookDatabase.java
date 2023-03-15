a160ba7b25c8c69b5d681e81bf4cb862110317fe
Madison Thompson <53984561+madisonthompson27@users.noreply.github.com>
2023-03-14 20:27:50 -0400

    Update BookDatabase.java
    Completed class

--- BookDatabase.java
+++ BookDatabase.java
@@ -8,17 +8,83 @@ import java.util.ArrayList;
 public class BookDatabase {
 
 	// declaring variables
-	private ArrayList<Book> books;
+	private final ArrayList<Book> books;
 
 	// creating blank constructor
 	public BookDatabase() {
-		// FIXME
-		int x = -1;
+		this.books = new ArrayList<>(); // will create a blank list if no books are given as input
 	}
 	
-	public BookDatabase(ArrayList<Book>) {
-		//FIXME
-		int y = -1;
+	// creating a constructor for when information is given
+	public BookDatabase(ArrayList<Book> books) {
+		this.books = books; // initializing the list with the values given
+	}
+	
+	// method to add a book to the given ArrayList
+	public void addBook(Book book) {
+		this.books.add(book); 
+	}
+	
+	// method to remove a book from the ArrayList
+	public void removeBook(Book book) {
+		this.books.remove(book);
+	}
+	
+	
+	// search methods
+	
+	// method to allow the user to search by author
+	public ArrayList<Book> search(Author author) {
+		
+		ArrayList<Book> result = new ArrayList<>();
+		
+		// for loop checks all books
+		for (Book book : books) {
+			if (book.author.equals(author)) { //FIXME
+				result.add(book);
+			} // if statement
+		} // for loop
+		
+		return result; // stores search results for user
+	} // author search method
+	
+	
+	// method to search by a range of years
+	public ArrayList<Book> search(int startYear, int endYear) {
+		
+		// creating new and separate list so that results from previous searches aren't included in new search
+		ArrayList<Book> result = new ArrayList<>();
+		for (Book book : books) {
+			if (book.year >= startYear && book.year <= endYear) { //FIXME
+				result.add(book);
+			} // if statement 
+		} // for loop 
+		
+		return result; // stores search results for user
+	} 
+	
+	
+	// method to search by genre
+	public ArrayList<Book> genre(String genre) {
+		
+		ArrayList <Book> result = new ArrayList<>();
+		for (Book book : books) {
+			if (book.genre.equals(genre)) {
+				result.add(book);
+			} // if statement
+		} // for loop
+		
+		return result; // stores search results for user
+	}
+	
+	
+	// to string method to allow books to be in format that can be read by user
+	public String toString() {
+		
+		// converts to a string that lists each book under the title "BookDatabase"
+		return "BookDatabase{ " +
+				"Books: " + books.toString() +
+				"}";
 	}
 	
 } // class BookDatabase
